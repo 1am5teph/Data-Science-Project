fe63d34358e9d1aeb0ff2c9768da492df2a70c16
1am5teph <marson.stephanie@gmail.com>
2016-02-28 12:59:44 -0800

    dictionary of post title and time stamp

--- reddit-api.py
+++ reddit-api.py
@@ -6,11 +6,11 @@ Created on Wed Feb 17 20:56:40 2016
 """
 # import json
 # import requests
-#
-# # Get Data
+
+# Get Data
 # r = requests.get("https://reddit/r/portland/api/")
 # r.text
-# #
+
 # ## Convert to dictionary
 # data = json.loads(r.text)
 # print(data)
@@ -20,31 +20,41 @@ import sys
 import json
 from datetime import datetime
 
-submissions = []
-
 r = praw.Reddit(user_agent='Post Subject on r/Portland')
-#works
-#username = input('username: ')
-#password = input('password: ')
-#r.login(username, password, disable_warning=True)
+# works
+# username = input('username: ')
+# password = input('password: ')
+# r.login(username, password, disable_warning=True)
 
-#pick a subreddit
+# pick a subreddit
 print('hello! i will compile reddit submissions.')
 subreddit = r.get_subreddit(input('pick a subreddit: '))
-posts = []
 
+# create post dictionary with title, points, and timestamp
+postdic = {}
+
+# Get New Subreddit Posts
 for post in subreddit.get_new(limit=2):
+    # Get Post ID
     postID = str(post.id)
-    posts.append(postID)
+    # Get Post Title and Total Points
     titlePoints = r.get_submission(submission_id = postID)
-    print(titlePoints)
+    # print(titlePoints)
+    # Get Post Timestamp
     SubmissionTimeStamp = datetime.fromtimestamp(titlePoints.created_utc)
-    print(SubmissionTimeStamp)
+    # print(SubmissionTimeStamp)
+    #Get Post Comments
     CommentID = praw.helpers.flatten_tree(titlePoints.comments)
     for comment in CommentID:
-        print(str(comment.body))
+        postComments = []
+        commentText = str(comment.body)
+        # print(commentText)
+        # Get Comment Timestamp
         CommentTimeStamp = datetime.fromtimestamp(comment.created_utc)
-        print(CommentTimeStamp)
+        # print(CommentTimeStamp)
+    postdic.update({str(titlePoints): str(SubmissionTimeStamp)})
+
+print(postdic)
 
 ##create dataset for specific time period
 #    #need title, dates, submissions, and count
